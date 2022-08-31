ec8ccdc4c6078ea8ebb249802e36363575082c58
Vivek <vivek9142@gmail.com>
2022-08-31 16:21:24 +0530

    added documentjs and added react portal configuration in app

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,27 +1,28 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import classes from './notification.module.css';
+import classes from "./notification.module.css";
 
 function Notification(props) {
   const { title, message, status } = props;
 
-  let statusClasses = '';
+  let statusClasses = "";
 
-  if (status === 'success') {
+  if (status === "success") {
     statusClasses = classes.success;
   }
 
-  if (status === 'error') {
+  if (status === "error") {
     statusClasses = classes.error;
   }
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById("notifications")
   );
 }
 
