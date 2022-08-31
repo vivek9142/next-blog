a0fd000f0142523404ea6e80c6cc26a2bc51bb92
Vivek <vivek9142@gmail.com>
2022-09-01 00:21:01 +0530

    added nxt config for dev and prod env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+const nextConfig = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      swcMinify: true,
+      env: {
+        DB: "nextBlog-dev",
+        DB_USER: "Vivek",
+      },
+    };
+  }
+  return {
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+      DB: "nextBlog",
+      DB_USER: "Vivek",
+    },
+  };
+};
+
+module.exports = nextConfig;
