5891682236e2ff2cf6427cf4b2b1a60d06d50b19
Vivek <vivek9142@gmail.com>
2022-08-31 15:29:47 +0530

    added db connection with backend

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -19,7 +21,28 @@ export default function handler(req, res) {
       name,
       message,
     };
+    let client;
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gnmor.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database" });
+      return;
+    }
 
+    const db = client.db();
+    let result;
+    try {
+      result = await db.collection("contact").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
     console.log(newMessage);
 
     res
